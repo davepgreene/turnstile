52488dc3778bd166c5902c6de1a6f57232a41072
dgreene-r7 <dgreene@rapid7.com>
2017-04-11 12:30:54 -0400

    Moved date parsing func to its own file. There might be more later and it's better to organize early.

--- utils/utils.go
+++ utils/utils.go
@@ -25,13 +25,3 @@ func MapKeys(m map[string]interface{}) []string {
 	}
 	return strkeys
 }
-
-func MsToTime(ms string) (time.Time, error) {
-	msInt, err := strconv.ParseInt(ms, 10, 64)
-	if err != nil {
-		return time.Time{}, err
-	}
-
-	return time.Unix(0, msInt*int64(time.Millisecond)), nil
-}
-
